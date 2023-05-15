eb35a2dd08889ba2a9523a567ab1e4e94cdc20b0
Sultan <sultanhoodie@gmail.com>
2023-05-15 13:01:14 +0800

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './Styles/Main.css';
+import {useState, useEffect, createContext} from 'react';
+import Header from './Components/Header';
+export const LanguageContext = createContext([])
+
 function App() {
+  const [rus, setRus] = useState(true)
+
   return (
-    <>
-    
-    </>
+    <LanguageContext.Provider  value={[rus, setRus]}>
+      <>
+       <Header/>
+      </>
+    </LanguageContext.Provider>
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,30 @@
+import { LanguageContext } from "../App";
+import { useContext } from 'react';
 export default function Header(){
+    const [rus, setRus] = useContext(LanguageContext)
+    const handleLngChange = (e) => {
+        if(e.target.value === 'option1'){
+            setRus(true)
+        }else{
+            setRus(false)
+        }
+    }
     return(
         <header className="main-header">
-            <div className="left-section"></div>
-            <div className="right-section"></div>
+            <div className="left-section">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Wikimedia-logo.png"></img>
+                <p>
+                    { rus ? "Делаем качественный ремонт квартир 'под ключ' в Санкт Петербурге, без предоплаты, c гарантией в договоре 5 лет" : 
+                    "Biz Sankt-Peterburgda yuqori sifatli kalit taslim bo'lgan kvartirani oldindan to'lovsiz, shartnomada 5 yil kafolat bilan ta'mirlaymiz." }</p>
+            </div>
+            <div className="right-section">
+                <button>{ rus ? "Цены + примеры сметы" : "Narxlar + taxminiy misollar"}</button>
+                <button>{ rus ? "Связаться с прорабом" : "Ustaga murojaat qiling"}</button>
+                <select id="options" onChange={handleLngChange}>
+                    <option value={'option1'}>Русский язык</option>
+                    <option value={'option2'}>Ozbek tili</option>
+                </select>
+            </div>
         </header>
     )
 }
