3dbf7747bf6317c82bb37316ea22662f1b8b0c9a
Sult Kh <sultkh@Sults-MacBook-Air.local>
2023-05-15 11:45:46 +0800

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <header className="main-header">
+            <div className="left-section"></div>
+            <div className="right-section"></div>
+        </header>
+    )
+}
