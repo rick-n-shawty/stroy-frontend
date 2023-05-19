6a9ea314bc92c81dedce48aae183e0479e05df0a
Sultan <sultanhoodie@gmail.com>
2023-05-19 13:54:31 +0800

    scroll

--- src/Components/PortfolioCard.js
+++ src/Components/PortfolioCard.js
@@ -2,15 +2,13 @@ import {Swiper, useSwiper, SwiperSlide} from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
 import "swiper/swiper-bundle.min.css";
-import "swiper/swiper.css"; 
 
 export default function PortfolioCard(){
     return(
         <div className="portfolio-card">
             <div className="image-container">
                 <div className="main-image">
-                <Swiper
-                        className='scroll'
+                    <Swiper
                         modules={[Navigation, Scrollbar, Pagination]}
                         spaceBetween={50}
                         slidesPerView={1}
@@ -30,9 +28,11 @@ export default function PortfolioCard(){
                 <div className='sub-images'>
                     <img src="https://i.1.creatium.io/2f/c7/81/c7760104c5f97a2b99b2d52fb86be61d20/qJI3kPEa_zs.jpg#%7B%22size%22:%5B1280,855%5D,%22quality%22:8%7D"></img>
                     <img src='https://i.1.creatium.io/d8/28/87/f5b3e8a689fe4d08f05dd0b7ce26849103/z5TC-Q--haI.jpg#%7B%22size%22:%5B1280,855%5D,%22quality%22:8%7D'></img>
+                    <img src='https://i.1.creatium.io/d8/28/87/f5b3e8a689fe4d08f05dd0b7ce26849103/z5TC-Q--haI.jpg#%7B%22size%22:%5B1280,855%5D,%22quality%22:8%7D'></img>
+                    <img src='https://i.1.creatium.io/d8/28/87/f5b3e8a689fe4d08f05dd0b7ce26849103/z5TC-Q--haI.jpg#%7B%22size%22:%5B1280,855%5D,%22quality%22:8%7D'></img>
                 </div>
             </div>
-            
+
             <div className="text-container">
                 <h1>Title</h1>
                 <p>Description dajkdhkajhdkjhadjkhkajdhkahjd</p>
